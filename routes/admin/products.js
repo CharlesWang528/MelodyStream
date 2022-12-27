53a90470a4e174deee48a43c7a77dcc0168e7ac9
CharlesWang528 <68451307+CharlesWang528@users.noreply.github.com>
2022-12-27 15:33:35 -0800

    Finish delete feature for admins
    just another post request

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -32,6 +32,7 @@ router.post('/admin/products/new',requireAuth, upload.single('image'),[requireTi
     const {title, price} = req.body;
     await productsRepo.create({title, price, image});
 
+
     res.redirect('/admin/products');
 
 
@@ -44,6 +45,38 @@ router.get('/admin/products/:id/edit', requireAuth, async (req, res) =>{
         return res.send('Product not found');
     }
     res.send(productsEditTemplate({product}));
+});
+
+router.post(
+    '/admin/products/:id/edit', 
+    requireAuth,upload.single('image'), 
+    [requireTitle,requirePrice],
+    handleErrors(productsEditTemplate, async (req) =>{
+        const product = await productsRepo.getOne(req.params.id);
+        return {product};
+    }),
+     async (req, res) =>{
+
+    const changes = req.body;
+    if (req.file){
+        changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+        await productsRepo.update(req.params.id, changes);
+    }catch(err){
+        return res.send('Could not find item')
+    }
+    res.redirect('/admin/products');
+
+});
+
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) =>{
+
+    await productsRepo.delete(req.params.id);
+
+    return res.redirect('/admin/products');
+
 });
 
 module.exports = router;
