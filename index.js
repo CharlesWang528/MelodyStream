2b3398a94234d14b3a4d757a2c967fb6d1284d9d
CharlesWang528 <68451307+CharlesWang528@users.noreply.github.com>
2022-12-26 17:21:09 -0800

    some work on image upload

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}))  //if we want all route handlers to have this middleware function to be applied
 app.use(cookieSession({
     keys:['randomstring1693']
 }));
 app.use(authRouter);
+app.use(productRouter);
 
-app.listen(3003, ()=>{
+app.listen(3001, ()=>{
     console.log("listening")
 });
