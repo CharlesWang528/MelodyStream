6f6136453ec7d206cbae134b78bb171c02766679
CharlesWang528 <68451307+CharlesWang528@users.noreply.github.com>
2022-12-27 16:17:00 -0800

    main page for user

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productRouter = require('./routes/admin/products');
+const adminroductRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(cookieSession({
     keys:['randomstring1693']
 }));
 app.use(authRouter);
-app.use(productRouter);
+app.use(adminroductRouter);
+app.use(productsRouter);
 
 app.listen(3003, ()=>{
     console.log("listening")
---
+++ routes/products.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const productsRepo = require('../repositories/products');
+const productIndexTemplage = require('../views/products/index');
+
+const router = express.Router();
+
+router.get('/', async (req, res) =>{
+
+    const products = await productsRepo.getAll();
+    res.send(productIndexTemplage({products}));
+
+});
+
+module.exports = router;
